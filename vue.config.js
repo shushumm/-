f3841f6c4cb3d2513b1f4341bc0ba56fd1a4fc25
fuliwei <1250993058@qq.com>
2020-02-18 11:00:56 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+    runtimeCompiler: true
+}
+
+
+let path = require('path');
+
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+
+module.exports = {
+  configureWebpack: {// 当前配置需要写入该选项中
+    resolve: {
+      extensions: ['.js', '.vue', '.json'],
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js', 
+        '@': resolve('src'), // 设置文件查找路径，可简写文件查找路径
+        'components': resolve('src/components'),
+      }
+    }
+  }
+}
+const px2rem = require('postcss-px2rem')
+// 配置postcs-px2rem
+const postcss = px2rem({
+  remUnit: 37.5   //基准大小 baseSize，需要和rem.js中单位rem值占比一样相同
+})
+css: { // 添加postcss配置
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          postcss
+        ]
+      }
+    }
+},
